f6738f35f2f23eb5e641da28a4c15a28215580a3
kailermai <133531148+kailermai@users.noreply.github.com>
2025-09-22 22:24:22 +0800

    Add JUnit test for remark command

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -6,7 +6,10 @@ import seedu.address.model.Model;
  * Changes the remark of an existing person in the address book.
  */
 public class RemarkCommand extends Command {
+
     public static final String COMMAND_WORD = "remark";
+
+    public static String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
     @Override
     public CommandResult execute(Model model) {
