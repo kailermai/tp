59f53fc203d9f8fcfea1c8b74fd43d9a83e4cd37
kailermai <133531148+kailermai@users.noreply.github.com>
2025-09-22 22:57:23 +0800

    Add comments for remark command parser

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -9,7 +9,15 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new {@code RemarkCommand} object
+ */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
+     * and returns a {@code RemarkCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     @Override
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
