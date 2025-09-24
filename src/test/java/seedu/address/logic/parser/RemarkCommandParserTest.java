65a717ba95a7ec3243a3729ae5dec0cde1426e34
kailermai <133531148+kailermai@users.noreply.github.com>
2025-09-24 16:08:03 +0800

    Add JUnit test for remark command parser

---
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.person.Remark;
+
+public class RemarkCommandParserTest {
+    private RemarkCommandParser parser = new RemarkCommandParser();
+    private final String nonEmptyRemark = "test";
+
+    @Test
+    public void parse_indexSpecified_success() {
+        // have remark
+        Index targetIndex = INDEX_FIRST_PERSON;
+        String input = targetIndex.getOneBased() + " " + PREFIX_REMARK + nonEmptyRemark;
+        RemarkCommand expected = new RemarkCommand(targetIndex, new Remark(nonEmptyRemark));
+        assertParseSuccess(parser, input, expected);
+
+        // no remark
+        input = targetIndex.getOneBased() + " " + PREFIX_REMARK;
+        expected = new RemarkCommand(targetIndex, new Remark(""));
+        assertParseSuccess(parser, input, expected);
+    }
+}
