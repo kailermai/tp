9207ae9f2662629c6ea6933bbd551d5aa1bb7b6e
kailermai <133531148+kailermai@users.noreply.github.com>
2025-09-24 16:54:12 +0800

    Add JavaDoc comment for remark model

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -2,9 +2,18 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
