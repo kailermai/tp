698fa44bfffdad31af860ddffd9f9d12d39fd079
lqr1019 <liqingrui1019@foxmail.com>
2025-10-08 17:45:31 +0800

    Add Submission class

--- src/main/java/seedu/address/model/record/Submission.java
+++ src/main/java/seedu/address/model/record/Submission.java
@@ -3,28 +3,46 @@ package seedu.address.model.record;
 import seedu.address.commons.core.index.Index;
 
 /**
- * Class for student's submission record
+ * Represents a student's submission record for weekly assessments.
  */
 public class Submission extends Record{
-
     /**
-     * Constructor for submission class
-     * @param MaxScore The maximum score that can be recorded for any week.
-     *                  This value is used to initialize the maximum allowed score for the submission record.
+     * Constructs a {@code Submission} instance with the specified maximum score.
+     * @param maxScore The maximum score that can be recorded for any week.
      */
-    public Submission (int MaxScore) {
-        super(MaxScore);
+    public Submission(int maxScore) {
+        super(maxScore);
     }
 
+    /**
+     * Returns the maximum score that can be recorded for any week.
+     *
+     * @return the maximum possible score
+     */
+    @Override
+    public int getMaxScore() {
+        return super.getMaxScore();
+    }
+
+    /**
+     * Returns the submission score for the specified week.
+     *
+     * @param index the week index for which the score is retrieved
+     * @return the submission score for that week
+     */
+    @Override
     public int getScore(Index index) {
         return super.getScore(index);
     }
 
+    /**
+     * Updates the submission score for the specified week.
+     *
+     * @param index the week index whose score is to be updated
+     * @param score the new score to record for that week
+     */
+    @Override
     public void setScore(Index index, int score) {
         super.setScore(index, score);
-    }
-
-    public int getMaxScore() {
-        return super.getMaxScore();
     }
 }
