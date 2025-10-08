a4e0e814dcf03822155ccff03c8137df8eb08657
jarretphon <jarretphon@gmail.com>
2025-10-09 00:03:23 +0800

    Create AttendanceScore class to represent weekly attendance

---
+++ src/main/java/seedu/address/model/record/AttendanceScore.java
@@ -0,0 +1,31 @@
+package seedu.address.model.record;
+
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents an attendance score for an attendance record..
+ * A valid week number is a positive integer between {@value MIN_SCORE} and {@value MAX_SCORE}, inclusive.
+ */
+public class AttendanceScore {
+    public static final int MIN_SCORE = 0;
+    public static final int MAX_SCORE = 1;
+    public static final String MESSAGE_CONSTRAINTS = "Only values from "
+            + MIN_SCORE + " to " + MAX_SCORE + "are allowed.";
+
+    public final int score;
+
+    /**
+     * Constructs an {@code AttendanceScore} object with the given score.
+     *
+     * @param attendanceScore The score to be assigned.
+     * @throws IllegalArgumentException If the provided week number is not within the valid range.
+     */
+    public AttendanceScore(int attendanceScore) {
+        this.score = attendanceScore;
+        checkArgument(isValidAttendanceScore(score), MESSAGE_CONSTRAINTS);
+    }
+
+    public static boolean isValidAttendanceScore(int attendanceScore) {
+        return attendanceScore >= MIN_SCORE && attendanceScore <= MAX_SCORE;
+    }
+}
