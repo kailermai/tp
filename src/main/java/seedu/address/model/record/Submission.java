0fc15376fa0fc0a363c99e995a729ef2a8da4884
lqr1019 <liqingrui1019@foxmail.com>
2025-10-08 19:58:22 +0800

    Remove redundant getters and setters

--- src/main/java/seedu/address/model/record/Submission.java
+++ src/main/java/seedu/address/model/record/Submission.java
@@ -14,35 +14,4 @@ public class Submission extends Record {
         super(maxScore);
     }
 
-    /**
-     * Returns the maximum score that can be recorded for any week.
-     *
-     * @return the maximum possible score
-     */
-    @Override
-    public int getMaxScore() {
-        return super.getMaxScore();
-    }
-
-    /**
-     * Returns the submission score for the specified week.
-     *
-     * @param index the week index for which the score is retrieved
-     * @return the submission score for that week
-     */
-    @Override
-    public int getScore(Index index) {
-        return super.getScore(index);
-    }
-
-    /**
-     * Updates the submission score for the specified week.
-     *
-     * @param index the week index whose score is to be updated
-     * @param score the new score to record for that week
-     */
-    @Override
-    public void setScore(Index index, int score) {
-        super.setScore(index, score);
-    }
 }
