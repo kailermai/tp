3417c751fa0ff7d7e14c7fcea5214a23a8e7d44f
lqr1019 <liqingrui1019@foxmail.com>
2025-10-09 14:51:39 +0800

    Add submissionRecords array in RecordList class

--- src/main/java/seedu/address/model/recordlist/RecordList.java
+++ src/main/java/seedu/address/model/recordlist/RecordList.java
@@ -3,6 +3,7 @@ package seedu.address.model.recordlist;
 import java.util.ArrayList;
 
 import seedu.address.model.record.AttendanceRecord;
+import seedu.address.model.record.SubmissionRecord;
 
 /**
  * Represents the list of all types of records, namely Attendance, Submission and Participation records.
@@ -10,7 +11,7 @@ import seedu.address.model.record.AttendanceRecord;
 public class RecordList {
     private static final int WEEKS = 13;
     private ArrayList<AttendanceRecord> attendanceRecords;
-    // private ArrayList<SubmissionRecord> submissionRecords;
+    private ArrayList<SubmissionRecord> submissionRecords;
     // private ParticipationList<ParticipationRecord> participationRecords;
 
     /**
@@ -18,8 +19,16 @@ public class RecordList {
      */
     public RecordList() {
         attendanceRecords = new ArrayList<>();
-        // instantiate submissionRecords
+        submissionRecords = new ArrayList<>();
         // instantiate participationRecords
+    }
+
+    public ArrayList<SubmissionRecord> getSubmissionRecords() {
+        return this.submissionRecords;
+    }
+
+    public void setSubmissionRecords(ArrayList<SubmissionRecord> submissionRecords) {
+        this.submissionRecords = submissionRecords;
     }
 
 
