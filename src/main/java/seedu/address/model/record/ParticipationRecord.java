b1da767158d2f783c3a6c2753db5acf14f60a390
Jerrytys <jerrytys03@gmail.com>
2025-10-09 12:03:23 +0800

    Integrate ParticipationScore into ParticipationRecord

--- src/main/java/seedu/address/model/record/ParticipationRecord.java
+++ src/main/java/seedu/address/model/record/ParticipationRecord.java
@@ -1,35 +1,28 @@
 package seedu.address.model.record;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 
 public class ParticipationRecord {
 
     static final int MAX_SCORE = 5;
-    private int score;
+    private final ParticipationScore participationScore;
 
     /**
      * Constructs a Participation object with the given score and remarks.
      *
      * @param score The participation score (0 to MAX_SCORE).
-     * @param remarks Additional remarks about the participation.
      * @throws IllegalArgumentException if the score is out of range.
      */
-    public ParticipationRecord(int score, String remarks) throws IllegalValueException {
-        if (score < 0 || score > MAX_SCORE) {
-            throw new IllegalValueException("Score must be between 0 and " + MAX_SCORE);
-        }
-        this.score = score;
+    public ParticipationRecord(ParticipationScore score) {
+        this.participationScore = score;
     }
 
     public int getScore() {
-        return score;
+        return participationScore.getScore();
     }
 
-
-    
     @Override
     public String toString() {
-        return "Score: " + score;
+        return "Score: " + participationScore.getScore();
     }
 
 
