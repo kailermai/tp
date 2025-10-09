3f2ab7d1860244eada671b542487a5a85fa608f8
jarretphon <jarretphon@gmail.com>
2025-10-10 00:20:47 +0800

    Remove unused record classes

--- src/main/java/seedu/address/logic/parser/RecordCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RecordCommandParser.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE_SCORE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PARTICIPATION_SCORE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SCORE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBMISSION_SCORE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_WEEK_NUMBER;
 
