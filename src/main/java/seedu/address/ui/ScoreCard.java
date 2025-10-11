91d46dfdd4e583ffc9bcd818f5a8b44b1f28e0ef
Jerrytys <jerrytys03@gmail.com>
2025-10-11 17:43:21 +0800

    Create ScoreCard and ScoreListPanel classes

--- src/main/java/seedu/address/ui/ScoreCard.java
+++ src/main/java/seedu/address/ui/ScoreCard.java
@@ -1,4 +1,36 @@
 package seedu.address.ui;
 
-public class ScoreCard {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+
+import seedu.address.model.record.Record;
+/**
+ * An UI component that displays information of a {@code Record}.
+ */
+public class ScoreCard extends UiPart<Region> {
+
+    private static final String FXML = "ScoreCard.fxml";
+
+    public final Record record;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label weekNumber;
+    @FXML
+    private Label recordImage;
+
+    /**
+     * Creates a {@code ScoreCard} with the given {@code Record} to display.
+     */
+    public ScoreCard(Record record, int weekNumber) {
+        super(FXML);
+        this.record = record;
+        this.weekNumber.setText("Week " + weekNumber);
+        this.recordImage.setText(record.toString());
+    }
+
 }
--- src/main/java/seedu/address/ui/ScoreListPanel.java
+++ src/main/java/seedu/address/ui/ScoreListPanel.java
@@ -1,4 +1,63 @@
 package seedu.address.ui;
 
-public class ScoreListPanel {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import seedu.address.commons.core.LogsCenter;
+
+import javafx.scene.layout.Region;
+import javafx.scene.control.ListView;
+import seedu.address.model.recordlist.RecordList;
+import seedu.address.model.record.Record;
+import seedu.address.model.student.Student;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ScoreListPanel extends UiPart<Region> {
+    private static final String FXML = "ScoreListPanel.fxml";
+    private static final Logger logger = LogsCenter.getLogger(ScoreListPanel.class);
+
+    @FXML
+    private ListView<Record> recordListView;
+    /**
+     * Creates a {@code ScoreListPanel} with the given {@code ObservableList}.
+     */
+    public ScoreListPanel() {
+        super(FXML);
+    }
+
+    public ScoreListPanel(RecordList recordList) {
+        super(FXML);
+        setRecordList(recordList);
+    }
+
+    public ScoreListPanel(Student student) {
+        super(FXML);
+        setRecordList(student.getRecordList());
+    }
+    private void setRecordList(RecordList recordList) {
+        ObservableList<Record> items = FXCollections.observableArrayList();
+        List<Record> records = recordList.records;
+        for (int i = 0; i < records.size(); i++) {
+            items.add(records.get(i));
+        }
+        recordListView.setItems(items);
+        recordListView.setCellFactory(listView -> new ScoreListPanel.ScoreListViewCell());
+    }
+
+    class ScoreListViewCell extends ListCell<Record> {
+        @Override
+        protected void updateItem(Record record, boolean empty) {
+            super.updateItem(record, empty);
+
+            if (empty || record == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new ScoreCard(record, getIndex() + 1).getRoot());
+            }
+        }
+    }
 }
