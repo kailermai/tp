1c870aad1a37fe912d0d2aa7cfa99bd05d8c88fb
jarretphon <jarretphon@gmail.com>
2025-10-18 23:22:43 +0800

    Refactor conditional colour rendering

--- src/main/java/seedu/address/ui/RecordDisplay.java
+++ src/main/java/seedu/address/ui/RecordDisplay.java
@@ -3,12 +3,12 @@ package seedu.address.ui;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import seedu.address.model.record.AttendanceScore;
 import seedu.address.model.record.ParticipationScore;
-import seedu.address.model.record.Score;
 import seedu.address.model.record.SubmissionScore;
 import seedu.address.model.record.WeekNumber;
 
@@ -17,12 +17,15 @@ import seedu.address.model.record.WeekNumber;
 */
 public class RecordDisplay extends HBox {
 
-    private static final int BOX_WIDTH = 30;
-    private static final int BOX_HEIGHT = 20;
+    private static final int BOX_WIDTH = 50;
+    private static final int BOX_HEIGHT = 40;
+    private static final int STROKE_WIDTH = 1;
+    private static final int BORDER_RADIUS = 8;
 
-    private static final Color COLOR_DEFAULT = Color.LIGHTGRAY;
-    private static final Color COLOR_SCORE_HIGH = Color.LIMEGREEN;
-    private static final Color COLOR_SCORE_MEDIUM = Color.YELLOWGREEN;
+    private static final Color COLOR_BORDER = Color.DIMGRAY;
+    private static final Color COLOR_DEFAULT = Color.DARKGRAY;
+    private static final Color COLOR_SCORE_HIGH = Color.SEAGREEN;
+    private static final Color COLOR_SCORE_MEDIUM = Color.ORANGE;
     private static final Color COLOR_SCORE_LOW = Color.TOMATO;
 
     private final StackPane[] scoreBoxes;
@@ -37,9 +40,15 @@ public class RecordDisplay extends HBox {
     public RecordDisplay() {
         this.scoreBoxes = new StackPane[WeekNumber.MAX_WEEK_NUMBER];
 
-        for (int i = 0; i < WeekNumber.MAX_WEEK_NUMBER; i++) {
+        for (int i = 0; i < scoreBoxes.length; i++) {
             Label scoreLabel = new Label("");
+
             Rectangle box = new Rectangle(BOX_WIDTH, BOX_HEIGHT, COLOR_DEFAULT);
+            box.setArcHeight(BORDER_RADIUS);
+            box.setArcWidth(BORDER_RADIUS);
+            box.setStroke(COLOR_BORDER);
+            box.setStrokeWidth(STROKE_WIDTH);
+
             StackPane boxPane = new StackPane(box, scoreLabel);
             scoreBoxes[i] = boxPane;
 
@@ -48,12 +57,19 @@ public class RecordDisplay extends HBox {
     }
 
     /**
+     * Represents the type of score being displayed.
+     */
+    public enum ScoreType {
+        ATTENDANCE, SUBMISSION, PARTICIPATION
+    }
+
+    /**
      * Displays the scores in the {@code RecordDisplay}.
      * Each score is represented by a colored box and a label displaying the score value.
      *
      * @param scores An array of {@code Score} objects to be displayed.
      */
-    public void displayScores(Score[] scores) {
+    public void displayScores(Integer[] scores, ScoreType scoreType) {
 
         assert scores.length == scoreBoxes.length;
 
@@ -63,29 +79,34 @@ public class RecordDisplay extends HBox {
             Rectangle box = (Rectangle) boxPane.getChildren().get(0);
             Label scoreLabel = (Label) boxPane.getChildren().get(1);
 
-            box.setFill(setBoxColor(scores[i]));
-            if (scores[i] != null) {
-                scoreLabel.setText(String.valueOf(scores[i].getScore()));
-            }
+            Integer score = scores[i];
+            box.setFill(setBoxColor(score, scoreType));
+            scoreLabel.setText(score != null ? String.valueOf(score) : "");
         }
     }
 
-    private Color setBoxColor(Score score) {
-        if (score instanceof AttendanceScore) {
-            return score.getScore() == AttendanceScore.MAX_SCORE ? COLOR_SCORE_HIGH : COLOR_SCORE_LOW;
-        } else if (score instanceof SubmissionScore) {
-            return score.getScore() == SubmissionScore.MAX_SCORE ? COLOR_SCORE_HIGH : COLOR_SCORE_LOW;
-        } else if (score instanceof ParticipationScore) {
-            if (score.getScore() == ParticipationScore.MAX_SCORE) {
+    private Color setBoxColor(Integer score, ScoreType scoreType) {
+        if (score == null) {
+            return COLOR_DEFAULT;
+        }
+
+        switch (scoreType) {
+        case ATTENDANCE:
+            return score == AttendanceScore.MAX_SCORE ? COLOR_SCORE_HIGH : COLOR_SCORE_LOW;
+        case SUBMISSION:
+            return score == SubmissionScore.MAX_SCORE ? COLOR_SCORE_HIGH : COLOR_SCORE_LOW;
+        case PARTICIPATION:
+            if (score == ParticipationScore.MAX_SCORE) {
                 return COLOR_SCORE_HIGH;
-            } else if (score.getScore() == ParticipationScore.MIN_SCORE) {
+            } else if (score == ParticipationScore.MIN_SCORE) {
                 return COLOR_SCORE_LOW;
             } else {
                 return COLOR_SCORE_MEDIUM;
             }
-        } else {
+        default:
             return COLOR_DEFAULT;
         }
+
     }
 
 }
