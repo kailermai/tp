b930ace3cde9cfd4d4667b5e2b696abf6e6e10ad
Jerrytys <jerrytys03@gmail.com>
2025-10-19 15:47:17 +0800

    Update ScoreCard for ResultDisplay

--- src/main/java/seedu/address/ui/ScoreCard.java
+++ src/main/java/seedu/address/ui/ScoreCard.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.record.Record;
+import seedu.address.model.record.WeekNumber;
+import seedu.address.model.recordlist.RecordList;
+import seedu.address.ui.RecordDisplay.ScoreType;
+
 /**
  * A UI component that displays information of a {@code Record}.
  */
@@ -12,23 +16,44 @@ public class ScoreCard extends UiPart<Region> {
 
     private static final String FXML = "ScoreCard.fxml";
 
-    public final Record record;
+    private RecordList recordList;
+    private ScoreType scoreType;
 
     @FXML
     private HBox cardPane;
     @FXML
-    private Label weekNumber;
+    private RecordDisplay recordDisplay;
     @FXML
-    private Label recordImage;
+    private Label scoreTypeLabel;
 
     /**
      * Creates a {@code ScoreCard} with the given {@code Record} to display.
      */
-    public ScoreCard(Record record, int weekNumber) {
+    public ScoreCard(RecordList recordList, ScoreType scoreType) {
         super(FXML);
-        this.record = record;
-        this.weekNumber.setText("Week " + weekNumber);
-        this.recordImage.setText(record.toString());
+        this.recordList = recordList;
+        this.scoreType = scoreType;
+        setRecordDisplay(scoreType);
     }
 
+    public void setRecordDisplay(ScoreType scoreType) {
+        Integer[] scores;
+        switch (scoreType) {
+        case ATTENDANCE:
+            scores = recordList.getScores(Record::getAttendanceScore);
+            scoreTypeLabel.setText("Attendance Scores");
+            break;
+        case PARTICIPATION:
+            scores = recordList.getScores(Record::getParticipationScore);
+            scoreTypeLabel.setText("Participation Scores");
+            break;
+        case SUBMISSION:
+            scores = recordList.getScores(Record::getSubmissionScore);
+            scoreTypeLabel.setText("Submission Scores");
+            break;
+        default:
+            scores = new Integer[WeekNumber.MAX_WEEK_NUMBER];
+        }
+        recordDisplay.displayScores(scores, scoreType);
+    }
 }
