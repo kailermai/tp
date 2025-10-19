2e1b9af649e0e7318ed81caa1d471d4d1250d4ce
liawjarell <jarellliaw@gmail.com>
2025-10-20 01:04:40 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/ui/HelpPanel.java
+++ src/main/java/seedu/address/ui/HelpPanel.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
