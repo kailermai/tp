d812372fbdbbaae4ef25ddf7997d968a370c7e2c
lqr1019 <liqingrui1019@foxmail.com>
2025-10-20 16:46:31 +0800

    Add SortCommand.java

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class SortCommand extends Command {
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort all students base on attendance and participation.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_HELP = "Sort all students base on attendance and participation:\n"
+            + COMMAND_WORD;
+
+    public static final String SHOWING_HELP_MESSAGE = "Opened sort window.";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+    }
+}
