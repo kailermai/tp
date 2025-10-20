8d6b2fdccee0d5f00081ec8524f6af36b8710559
liawjarell <jarellliaw@gmail.com>
2025-10-20 17:20:02 +0800

    Change help message for readability

--- src/main/java/seedu/address/ui/HelpPanel.java
+++ src/main/java/seedu/address/ui/HelpPanel.java
@@ -7,6 +7,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import seedu.address.logic.commands.AddCommand;
@@ -49,10 +50,12 @@ public class HelpPanel extends UiPart<Region> {
     private ScrollPane helpScrollPane;
 
     @FXML
-    private TextFlow helpMessage;
+    private VBox helpMessage;
 
     @FXML
     private Button copyButton;
+
+    private static Text newLine = new Text("\n");
 
     /**
      * Creates a HelpPanel.
@@ -60,31 +63,30 @@ public class HelpPanel extends UiPart<Region> {
     public HelpPanel() {
         super(FXML);
         buildHelpMessage();
-//        helpMessage.setText(HELP_MESSAGE);
-//        helpMessage.setWrapText(true);
     }
 
     private void buildHelpMessage() {
         helpMessage.getChildren().addAll(
-            createHeader("Command list:\n\n"),
-            createCommand(HelpCommand.MESSAGE_HELP),
-            createCommand(AddCommand.MESSAGE_HELP),
-            createCommand(ListCommand.MESSAGE_HELP),
-            createCommand(EditCommand.MESSAGE_HELP),
-            createCommand(FindCommand.MESSAGE_HELP),
-            createCommand(DeleteCommand.MESSAGE_HELP),
-            createCommand(RecordCommand.MESSAGE_HELP),
-            createCommand(ClearCommand.MESSAGE_HELP),
-            createCommand(ExitCommand.MESSAGE_HELP),
-            createCommand(ViewCommand.MESSAGE_HELP),
-            createCommand(TrendCommand.MESSAGE_HELP),
+            createHeader("Command list:"),
+            createCommandTitleAndDescription(HelpCommand.MESSAGE_HELP_TITLE, HelpCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(AddCommand.MESSAGE_HELP_TITLE, AddCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(ListCommand.MESSAGE_HELP_TITLE, ListCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(EditCommand.MESSAGE_HELP_TITLE, EditCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(FindCommand.MESSAGE_HELP_TITLE, FindCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(DeleteCommand.MESSAGE_HELP_TITLE, DeleteCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(RecordCommand.MESSAGE_HELP_TITLE, RecordCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(ClearCommand.MESSAGE_HELP_TITLE, ClearCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(ExitCommand.MESSAGE_HELP_TITLE, ExitCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(ViewCommand.MESSAGE_HELP_TITLE, ViewCommand.MESSAGE_HELP_DESCRIPTION),
+            createCommandTitleAndDescription(TrendCommand.MESSAGE_HELP_TITLE, TrendCommand.MESSAGE_HELP_DESCRIPTION),
             createFooter("\n" + USERGUIDE_TEXT)
         );
     }
 
-    private Text createHeader(String text) {
-        Text header = new Text(text);
+    private Label createHeader(String text) {
+        Label header = new Label(text);
         header.getStyleClass().add("help-header");
+        header.setWrapText(true);
         return header;
     }
 
@@ -94,9 +96,34 @@ public class HelpPanel extends UiPart<Region> {
         return command;
     }
 
-    private Text createFooter(String text) {
-        Text footer = new Text(text);
+    private VBox createCommandTitleAndDescription(String title, String description) {
+        Label titleLabel = createCommandTitle(title);
+
+        Label descriptionLabel = createCommandDescription(description);
+
+        VBox command = new VBox(2, titleLabel, descriptionLabel);
+        command.getStyleClass().add("help-command-block");
+        return command;
+    }
+
+    private Label createCommandTitle(String text) {
+        Label command = new Label(text);
+        command.getStyleClass().add("help-command-title");
+        command.setWrapText(true);
+        return command;
+    }
+
+    private Label createCommandDescription(String text) {
+        Label command = new Label(text);
+        command.getStyleClass().add("help-command-description");
+        command.setWrapText(true);
+        return command;
+    }
+
+    private Label createFooter(String text) {
+        Label footer = new Label(text);
         footer.getStyleClass().add("help-footer");
+        footer.setWrapText(true);
         return footer;
     }
 
