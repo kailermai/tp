3eb9ba8415a114640ecd5f25924205e60b4f9e08
liawjarell <jarellliaw@gmail.com>
2025-10-28 17:51:37 +0800

    Change Name and AddCommand error message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -21,7 +21,7 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to TAHub.\n"
             + "Parameters: "
             + PREFIX_NAME + "NAME "
             + PREFIX_STUDENT_NUMBER + "STUDENT NUMBER "
